5936a08544d6272f98667dbdf618ef810729caaf
ADITYA748848 <adityaraj5138@gmail.com>
2025-03-28 11:44:15 +0530

    inchanse code

--- FORNTEND/pages/index.js
+++ FORNTEND/pages/index.js
@@ -1,4 +1,9 @@
 import Head from "next/head";
+import Link from "next/link";
+import  { BiDownload} from "react-icons/bi";
+import { FaGithub, FaTwitter } from "react-icons/fa6";
+import { GrLinkedinOption } from "react-icons/gr";
+import { LiaBasketballBallSolid } from "react-icons/lia";
 
 export default function Home() {
 
@@ -38,11 +43,84 @@ export default function Home() {
 
       {/* hero section */}
       <section className="hero">
+        <div className="intro_text">
+          <svg viewBox="0 0 1320 300">
+            <text x= '50%' y = '50%' textAnchor = 'middle' className="animate-stroke">HI</text>
+          </svg>
+
+        </div>
+        <div className="container">
+          <div className="flex w-100">
+            <div className="heroinfoleft">
+              <span className="hero_sb_title">I am Aditya</span>
+              <h1 className="hero_title">Web Devloper + <br/> <span className=" typed-text"> Ux Designer</span></h1>
+              <div className="hero_img_box heroimgbox">
+                <img src="/img/ me.jpg"alt="coder"/>
+                
+              </div> 
+              <div className=" lead"> I break down complex user experience problems to create integritiy focussed solution that connect billions of people .</div>
+              <div className=" hero_btn_box">
+                <Link href='/' download={'/img/resume.pdf'} className='download_cv'>Download CV <BiDownload/></Link>
+                <ul className="hero_social">
+                  <li> < a href="/"><FaTwitter/></a></li>
+                  <li> < a href="/"><LiaBasketballBallSolid/></a></li>
+                  <li> < a href="/"><GrLinkedinOption/></a></li>
+                  <li> < a href="/"><FaGithub/></a></li>
+
+                </ul>
+              </div>
+            </div>
+            {/* rightside image section */}
+            <div className="heroimageright">
+              <div className="hero_img_box">
+                <img src="/img/me.png" alt=""/>
+              </div>
+
+            </div>
+          </div>
+          <div className="funfect_area flex flex-sb">
+            <div className="funfect_item">
+              <h3>3+</h3>
+              <h4>year of<br/>
+                 Experience </h4>
+              
+            </div>
+            <div className="funfect_item">
+              <h3> 20+ </h3>
+              <h4> projects <br/>
+                 completed</h4>
+              
+            </div>
+            <div className="funfect_item">
+              <h3>12+</h3>
+              <h4> OpenSource <br/>
+                Library </h4>
+              
+            </div>
+            <div className="funfect_item">
+              <h3>40+</h3>
+              <h4>Happy <br/>
+              Customers</h4>
+              
+            </div>
+          
+          </div>
+          
+        </div>
       
       </section>
 
       {/* Services */}
       <section className="services">
+        <div className="services">
+          <div className="container">
+            <div className="services_titles">
+              <h2>MY Quality services</h2>
+              <p> We put your ideas and thus your wishes in the form of a unique web project that inspires you and you customers . </p>
+            </div>
+            <div className="services_menu"></div>
+          </div>
+        </div>
         
       </section>
 
